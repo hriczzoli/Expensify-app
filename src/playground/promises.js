59528af0dd7ec52791a734ec8de38e6366274882
hriczzoli <hriczzoli@gmail.com>
2019-08-04 16:33:58 +0100

    Setup test databse environment

---
+++ src/playground/promises.js
@@ -0,0 +1,22 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is my resolved data');
+        // reject('Something went wrong');
+    }, 1500);
+});
+
+console.log('before');
+
+promise.then(( data) => {
+    console.log('1', data);
+
+    return'some data';
+}).then((str) => {
+    console.log('Does this run?', str);
+}).catch((error) => {
+    console.log('error: ', error);
+});
+
+
+
+console.log('after');
